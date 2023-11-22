6c8cc13dd63f53e017901ed2819969fdb601c533
Miguel Mestre <mestremn28@hotmail.com>
2023-11-22 15:25:02 +0000

    lab3rdDay

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const logger = require("morgan");
 
 const app = express();
 
+app.use(express.urlencoded())
+
 // MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
+const Recipe = require("./models/Recipe.model.js")
+
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const mongoose = require("mongoose");
 
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 
 // ROUTES
@@ -21,25 +32,85 @@ app.get('/', (req, res) => {
 });
 
 
+
+
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", (req, res)=>{
+    Recipe.create({
+        title: req.body.title,
+        instructions: req.body.instructions,
+        level: req.body.level,
+        ingridients: req.body.ingridients,
+        image: req.body.image,
+        duration: req.body.duration,
+        isArchived: req.body.isArchived,
+        created: req.body.created,
+    })
+    .then((createdRecipe)=>{
+        console.log("Recipe is created", createdRecipe);
+        res.status(201).send(createdRecipe);
+    })
+    .catch((error)=>{
+        console.log(error);
+        res.status(500).send({error: 'Failed'})
+    })
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get('/recipes', (req, res)=>{
+    Recipe.find()
+    .then((recipes)=>{
+        res.status(200).send(recipes)
+    })
+    .catch((error)=>{
+        res.status(500).send({error: 'failed'})
+    })
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+app.get("/recipes/:recipeId", (req, res)=>{
+    
+
+    Recipe.findById(req.params.recipeId)
+    .then((recipe)=>{
+        res.status(200).send(recipe)
+    })
+    .catch((error)=>{
+        res.status(500).send({error: 'failed to get the sinle'})
+    })
+})
 
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
 
+app.put("/recipes/:recipeId", (req, res)=>{
+    Recipe.findByIdAndUpdate(req.params.recipeId, req.body, {new: true})
+    .then((updatedRecipes)=>{
+        res.status(200).send(updatedRecipes)
+    })
+    .catch((error)=>{
+        console.log(error)
+        res.status(500).send({error: 'failed to update'})
+    })
+})
+
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
+app.delete("/recipes/:recipeId", (req, res)=>{
+    Recipe.findByIdAndDelete(req.params.recipeId)
+    .then(()=>{
+        res.status(200).send({message: 'recipe deleted'})
+    })
+    .catch((error)=>{
+        res.status(500).send({message: 'failed to delete'})
+    })
+})
 
 // BONUS
 //  Bonus: Iteration 9 - Create a Single User
