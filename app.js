4f61be5458ef955119148442b01c6884ce5fc23b
“elisejonkers” <elisejonkers18@hotmail.com>
2023-11-29 17:47:27 +0100

    completed iterations 3, 4, and 5

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+const Recipe = require("./models/Recipe.model")
 const mongoose = require("mongoose")
-
-const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev"
-
-mongoose
-  .connect(MONGODB_URI)
-  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
-  .catch((err) => console.error("Error connecting to mongo", err));
 
 const app = express();
 
@@ -16,11 +10,14 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev"
 
-
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
 //  GET  / route - This is just an example route
@@ -28,18 +25,44 @@ app.get('/', (req, res) => {
     res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
 
-
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", (req, res) => {
+    Recipe.create(req.body)
+        .then((newRecipe) => {
+            res.status(201).json(newRecipe)
+        })
+        .catch((error) => {
+            console.log("Error creating a recipe: ", error)
+            res.send("Error creating a recipe")
+        })
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes", (req, res) => {
+    Recipe.find()
+        .then((recipesFromDB) => {
+            res.status(200).json(recipesFromDB)
+        })
+        .catch((error) => {
+            res.status(500).json({message: "Error getting the recipes"})
+        })
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+app.get("/recipes/:id", (req, res) => {
+    const {id} = req.params
 
+    Recipe.findById(id)
+        .then((recipeFromDB) => {
+            res.status(200).json(recipeFromDB)
+        })
+        .catch((error) => { 
+            res.status(500).json({message: "Error getting the recipe"})
+        })
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
