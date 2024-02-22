3be34061b75f16459d7c5ae0764aaebf9d85ef6a
pikkukurkku <143491239+pikkukurkku@users.noreply.github.com>
2024-02-22 09:34:23 +0100

    partly done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,13 @@
 // Your code here ...
+const {model, Schema} = require("mongoose");
+
+const recipeSchema = new Schema ((
+    title: {type: String, required: true, unique: true},
+instructions: {type: String, required: true},
+level : {type: String, enum: ["Easy Peasy", "Amateur"]}
+))
+
+
+const Recipe = model("Recipe", recipechema)
+
+export
