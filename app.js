e7c38be2ac2a40a5fabb1c7fbcd30414bf001ccf
Stefan Kummerlöw <gummiz@gmx.net>
2024-02-28 15:19:44 +0100

    fix(app.js): remove unnecessary empty line in app.js file feat(app.js): add support for creating a new recipe by implementing POST /recipes route feat(app.js): add support for retrieving all recipes by implementing GET /recipes route feat(models/Recipe.model.js): create Recipe model schema for MongoDB integration

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const logger = require("morgan");
 
 const app = express();
 
+
 // MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.static("public"));
@@ -12,6 +13,7 @@ app.use(express.json());
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
 const mongoose = require("mongoose")
+const Recipe = require("./models/Recipe.model.js") // Recipe schema
 
 const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
@@ -29,11 +31,36 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
+app.post("/recipes", (req, res, next) =>{
+    // console.log(req.body)
+    const {title, instructions, level, ingredients, image, duration, isArchived, created} = req.body
+    const newRecipe = {
+        title: title,
+        instructions: instructions,
+        level: level,
+        ingredients: ingredients,
+        image: image,
+        duration: duration,
+        isArchived: isArchived,
+        created: created
+    }
+
+    Recipe.create(newRecipe)
+    .then((recipe) => res.status(201).json(recipe))
+    .catch((err) => res.status(500).send(err));
+})
 
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes", (req, res, next) => {
+    Recipe.find()
+    .then((result) => {
+        res.status(200).json(result)
+    }).catch((err) => {
+        res.status(500).send(err)
+    });
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
