ccf6e30a1174b9a43a775e3d7422e1a508984b00
piyush <piyushbishthld123@gmail.com>
2024-02-28 18:03:04 +0100

    solved

--- app.js
+++ app.js
@@ -9,16 +9,28 @@ app.use(express.static("public"));
 app.use(express.json());
 
 
+
+app.use(express.urlencoded({extended:false}))
+
 // Iteration 1 - Connect to MongoDB
+const mongoose = require("mongoose")
+
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 // DATABASE CONNECTION
 
-
-
+const userRoutes = require("./routes/users.routes")
+app.use(userRoutes)
 // ROUTES
 //  GET  / route - This is just an example route
 app.get('/', (req, res) => {
     res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
+
 
 
 //  Iteration 3 - Create a Recipe route
