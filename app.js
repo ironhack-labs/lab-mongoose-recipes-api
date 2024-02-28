60d84778e043330601087c4b3f094b4f0cd782ae
Simona Wurst <simona_wu@yahoo.de>
2024-02-28 15:57:23 +0100

    First commit - solved until 5th iteration

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model.js")
 
 const app = express();
 
@@ -11,6 +13,12 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+    .connect(MONGODB_URI)
+    .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch((err) => console.error("Error connecting to mongo", err));
 
 
 
@@ -24,13 +32,63 @@ app.get('/', (req, res) => {
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
 
+app.post("/recipes", (req, res) => {
+
+    Recipe.create({
+        title: req.body.title,
+        instructions: req.body.instructions,
+        level: req.body.level,
+        ingredients: req.body.ingredients,
+        image: req.body.image,
+        duration: req.body.duration,
+        isArchived: req.body.isArchived,
+        created: req.body.created
+    })
+        .then((newRecipe) => {
+            // console.log(newRecipe);
+            res.status(200).json(newRecipe);
+        })
+        .catch((e) => {
+            // console.log(e);
+            res.status(500).json({ e: "Failed to create a new recipe" });
+        })
+})
+
+
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
+app.get("/recipes", (req, res) => {
+    Recipe.find({})
+        .then((recipeList) => {
+           // console.log(recipeList)
+            res.status(200).json(recipeList);
+        })
+        .catch((e) => {
+            // console.log(e);
+            res.status(500).json({ e: "Failed to display the list of all recipes" });
+        })
+
+})
+
 
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+
+app.get("/recipes/:id", (req, res) => {
+    Recipe.findById (
+      req.params.id
+    )
+        .then((recipeDetails) => {
+            console.log(recipeDetails)
+            res.status(200).json(recipeDetails);
+        })
+        .catch((e) => {
+            console.log(e);
+            res.status(500).json({ e: "Couldn't display details of this recipe" })
+        })
+})
 
 
 //  Iteration 6 - Update a Single Recipe
