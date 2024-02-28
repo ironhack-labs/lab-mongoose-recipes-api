733753fdac3c942ba36c8ded43fcb362383e7553
“Filipe.Santiago” <thekyd42@gmail.com>
2024-02-28 17:51:44 +0000

    Completed lab

--- app.js
+++ app.js
@@ -7,12 +7,18 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
-
+app.use(express.urlencoded({extended:false}));
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model")
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
-
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
 //  GET  / route - This is just an example route
@@ -23,15 +29,42 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
+app.post("/recipes", async (req, res)=>{
+   const {title, instructions, level, ingredients, image, duration, isArchived, created} = req.body;
 
+   try{
+    const newRecipe = await Recipe.create({title, instructions, level, ingredients, image, duration, isArchived, created});
+    res.status(201).json(newRecipe);
+   }
+   catch(error){
+    res.status(500).json({ message: "Error creating a new recipe!" });
+}
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes", async (req, res)=>{
+    try{
+    const allRecipes = await Recipe.find();
+    res.status(200).json(allRecipes);
+    }
+    catch(error){
+        res.status(500).json({ message: "Error!" });
+    }
+});
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get("/recipes/:id", async(req,res)=>{
+    try{
+    const {id} = req.params;
+    const recipe = await Recipe.findById(id);
+    res.status(200).json(recipe);
+    }
+    catch(error){
+        res.status(500).json({ message: "Error! No soup for you!" });
+    }
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,15 @@
-// Your code here ...
+const moongose = require("mongoose");
+const {Schema, model} = moongose;
+const url = "https://images.media-allrecipes.com/images/75131.jpg"
+
+const recipeSchema = new Schema({
+    title: {type: String, required: true, unique: true}, // string, required field
+    instructions : {type: String, required: true},
+    level: {type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]}, // String
+    ingredients : [String],
+    image :{type: String, default: url},
+    duration: {type: Number, min: 0},
+    isArchived: {type: Boolean, default: false},
+    created: {type: Date, default: Date.now}
+})
+module.exports = model("Recipe", recipeSchema)
