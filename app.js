a56655b3298e903c3bb274365e3c47615cc95c4a
karkelo <dilanpapuc@gmail.com>
2024-02-28 22:51:25 +0100

    done

--- app.js
+++ app.js
@@ -56,26 +56,49 @@ app.post("/recipes", (req, res) => {
 //  GET  /recipes route
 app.get('/recipes', (req, res) => {
     Recipe.find()
-    .then((allRecipes) =>{
-    res.status(200).json(allRecipes);
-})
-.catch((error) => {
-    res.status(500).json({ message: "Error while getting recipes" });
-});
+        .then((allRecipes) => {
+            res.status(200).json(allRecipes);
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Error while getting recipes" });
+        });
 });
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get('/recipes/:id', (req, res) => {
+    Recipe.findById(req.params.id)
+        .then((recipe) => {
+            res.status(200).json(recipe);
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Error while getting a single recipes" });
+        });
+});
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put('/recipes/:id', (req, res) => {
+    Recipe.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((updatedRecipe) => {
+            res.status(200).json(updatedRecipe);
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Error while getting a single recipes" });
+        });
+});
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
-
-
+app.delete("/recipes/:id", (req, res) => {
+    Recipe.findByIdAndDelete(req.params.id)
+        .then(() => {
+            res.status(204).send();
+        })
+        .catch((error) => {
+            res.status(500).json({ message: "Error while deleting a single recipes" });
+        });
+});
 // Start the server
 app.listen(3000, () => console.log('My first app listening on port 3000!'));
 
