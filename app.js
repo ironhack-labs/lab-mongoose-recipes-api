22aa224d3eb0f41e0fb725d7d594f10cbb77307c
pedro roca de togores <pedrobgrdt@gmail.com>
2024-02-28 22:02:33 +0100

    clean finish lab

--- app.js
+++ app.js
@@ -1,51 +1,90 @@
-const express = require("express");
-const logger = require("morgan");
+const express = require("express")
+const logger = require("morgan")
+const mongoose = require("mongoose")
+const Recipe = require("./models/Recipe.model")
 
-const app = express();
+const app = express()
 
-// MIDDLEWARE
-app.use(logger("dev"));
-app.use(express.static("public"));
-app.use(express.json());
+app.use(logger("dev"))
+app.use(express.static("public"))
+app.use(express.json())
 
+const databaseName = 'express-mongoose-recipes-dev'
+const connectionString = `mongodb://127.0.0.1:27017/${databaseName}`
 
-// Iteration 1 - Connect to MongoDB
-// DATABASE CONNECTION
-
-
+mongoose
+    .connect(connectionString)
+    .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
-//  GET  / route - This is just an example route
+
+const BASE_API_URL = 'http://localhost:3000'
+
 app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
-});
+    res.send("<h1>LAB | Express Mongoose Recipes</h1>")
+})
+
+app.post('/recipes', (req, res) => {
+    const { title, instructions, level, ingredients, image, duration, isArchived, created } = req.body
+
+    Recipe
+        .create(req.body)
+        .then(createdRecipe => res.status(201).json(createdRecipe))
+        .catch(err => res.status(500).json(err))
+})
+
+app.get('/recipes', (req, res) => {
+
+    Recipe
+        .find()
+        .then(allRecipes => res.status(200).json(allRecipes))
+        .catch(err => res.status(500).json(err))
+})
+
+app.get('/recipes/:id', (req, res) => {
+
+    const { id: recipeId } = req.params
 
 
-//  Iteration 3 - Create a Recipe route
-//  POST  /recipes route
+    Recipe
+        .findById(recipeId)
+        .then(recepiInfo => res.status(200).json(recepiInfo))
+        .catch(err => res.status(500).json(err))
+})
 
+app.put('/recipes/:id', (req, res) => {
 
-//  Iteration 4 - Get All Recipes
-//  GET  /recipes route
+    const { id: recipeId } = req.params
+    const { title, instructions, level, ingredients, image, duration, isArchived, created } = req.body
 
+    Recipe
 
-//  Iteration 5 - Get a Single Recipe
-//  GET  /recipes/:id route
+        .findByIdAndUpdate(
+            recipeId,
+            req.body,
+            { new: true, runValidators: true }
+        )
+        .then(updatedRecipe => res.status(200).json(updatedRecipe))
+        .catch(err => res.status(500).json(err))
+})
 
+app.delete('/recipes/:id', (req, res) => {
 
-//  Iteration 6 - Update a Single Recipe
-//  PUT  /recipes/:id route
+    const { id: recipeId } = req.params
+    Recipe
+        .findByIdAndDelete(recipeId)
+        .then(res.sendStatus(204))
+        .catch(err => res.status(500).json({ message: 'Borraste la base de datos!!!' }))
+})
 
+// Posible debate¿?¿?  If a DELETE method is successfully applied, the origin server SHOULD
+//    send a 202(Accepted) status code if the action will likely succeed
+//    but has not yet been enacted, a 204(No Content) status code if the
+//    action has been enacted and no further information is to be supplied,
+//     or a 200(OK) status code if the action has been enacted and the
+//    response message includes a representation describing the status.
 
-//  Iteration 7 - Delete a Single Recipe
-//  DELETE  /recipes/:id route
+app.listen(3000, () => console.log('My first app listening on port 3000!'))
 
-
-
-// Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
-
-//❗️DO NOT REMOVE THE BELOW CODE
-module.exports = app;
+module.exports = app
