fb8dec386dd68e275185f9282abcda4202d34288
SantosMatheusSilva <mathsantos52@gmail.com>
2024-02-29 09:10:32 +0000

    lab done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,57 @@
 // Your code here ...
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema ({
+
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    intructions: {
+        type: String,
+        required: true
+    },
+    level: {
+        type: String,
+        enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
+    },
+    ingredients: {
+        type: [String],
+    },
+    image: {
+        type: String,
+        default: "https://images.media-allrecipes.com/images/75131.jpg"
+    },
+    duration: {
+        type: Number,
+        min:0
+    },
+    isArchived: {
+        type: Boolean,
+        default: false
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+
+
+})
+
+const Recipe = mongoose.model("Recipe", recipeSchema);
+
+module.exports = Recipe;
+
+/* 
+title - Type String. It should be required and unique.
+instructions - Type String. It should be required.
+level - Type String. It can be one of the following values: Easy Peasy - Amateur Chef - UltraPro Chef (use the enum validator 😉).
+ingredients - Type Array of Strings - represented as [ String ].
+image - Type String. Default value: "https://images.media-allrecipes.com/images/75131.jpg".
+duration - Type Number. The minimum value should be 0.
+isArchived - Type Boolean. The default value should be false.
+created - Type Date. By default, today.
+
+*/
