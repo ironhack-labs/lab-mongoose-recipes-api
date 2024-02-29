d2acc171d30a3f623259e19be5e81c2325da97cb
Egbagba <egbagba02@gmail.com>
2024-02-29 14:54:50 +0000

    ESE

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
 const express = require("express");
 const logger = require("morgan");
 
+
 const app = express();
 
 // MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({extend:false}));
 
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+// app.js
+const mongoose = require("mongoose");
 
+mongoose
+    .connect("mongodb://127.0.0.1:27017/express-mongoose-recipes-dev")
+    .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch((err) => console.error("Error connecting to mongo", err));
+
+// ...
 
 
 // ROUTES
 //  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
-});
+const recipeRoutes = require('./routes/recipe.routes')
 
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.use('/api', recipeRoutes);
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
 
-
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,20 @@
-// Your code here ...
+const mongoose = require("mongoose");
+const {Schema, model} = mongoose;
+
+
+const recipeSchema = new Schema({
+  title: String,
+  instructions: String,
+  level: String,
+  ingredients: [ String ],
+  image: String,
+  duration: String,
+  isArchived: String,
+  created: {type: Date, default: Date.now}
+})
+
+module.exports = model("Recipe", recipeSchema);
+
+
+ 
+
