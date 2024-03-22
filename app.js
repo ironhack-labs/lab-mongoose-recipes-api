fcb875855a8767dbc4ab619cd5edfb1567b3145c
Irene <amoreperdesi@gmail.com>
2024-03-22 18:25:21 +0100

    Lab done

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const logger = require("morgan");
 
 const app = express();
 
+const mongoose=require("mongoose");
+
+const Recipe = require('./models/Recipe.model')
+
 // MIDDLEWARE
 app.use(logger("dev"));
 app.use(express.static("public"));
@@ -12,7 +16,12 @@ app.use(express.json());
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
 
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
 //  GET  / route - This is just an example route
@@ -24,23 +33,96 @@ app.get('/', (req, res) => {
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
 
+app.post("/recipes", (req,res) =>{
+
+    Recipe.create({
+        title:req.body.title,
+        instructions:req.body.instructions,
+        level:req.body.level,
+        ingredients:req.body.ingredients,
+        image:req.body.image,
+        duration:req.body.duration,
+        isArchived:req.body.isArchived,
+        created:req.body.created,
+    })
+    .then((createdRecipe)=>{
+        res.json(createdRecipe);
+    })
+    .catch((err)=>{
+        res.json(err);
+    })
+})
+
+app.get("/recipes", (req,res) =>{
+
+    Recipe.find()
+    .then((allRecipes)=>{
+        res.status(200).json(allRecipes);
+    })
+    .catch((err)=>{
+        res.status(500).json({message:"Error"});
+    })
+})
+
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
 
 
+app.get("/recipes", (req,res) =>{
+
+    Recipe.find()
+    .then((allRecipes)=>{
+        res.status(200).json(allRecipes);
+    })
+    .catch((err)=>{
+        res.status(500).json({message:"Error"});
+    })
+})
+
+
+
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
 
+app.get("/recipes/:id", (req,res) =>{
+
+    Recipe.findById(req.params.id)
+    .then((oneRecipe)=>{
+        res.status(200).json(oneRecipe);
+    })
+    .catch((err)=>{
+        res.status(500).json({message:"Error"});
+    })
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
 
+app.put("/recipes/:id", (req,res) =>{
+
+    Recipe.findByIdAndUpdate(req.params.id, req.body, {new:true})
+    .then((updatedRecipe)=>{
+        res.status(200).json(updatedRecipe);
+    })
+    .catch((err)=>{
+        res.status(500).json({message:"Error"});
+    })
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
+app.delete("/recipes/:id", (req,res) =>{
 
+    Recipe.findByIdAndDelete(req.params.id)
+    .then((deletedMovie)=>{
+        res.status(200).json(deletedMovie);
+    })
+    .catch((err)=>{
+        res.status(500).json({message:"Error"});
+    })
+})
 
 // Start the server
 app.listen(3000, () => console.log('My first app listening on port 3000!'));
