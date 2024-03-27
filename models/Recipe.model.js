32c4707df25124c9d489cfd7aacf25c2d09dbdf8
Alexandra Busson <alexandra_b@live.fr>
2024-03-27 15:10:42 +0100

    lab done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,23 @@
-// Your code here ...
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const RecipeSchema = new Schema({
+  title: String,
+  instructions: { type: String, require: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"],
+  },
+  ingredients: [String],
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: { type: Number, default: 0 },
+  isAchived: { type: Boolean, default: false },
+  created: { type: Date, default: Date.now },
+});
+
+const Recipe = mongoose.model("Recipe", RecipeSchema);
+
+module.exports = Recipe;
