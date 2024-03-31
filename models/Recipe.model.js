0811f12c86e58a3075bc3b3786de6b882c975ce0
onurcangulpak <onurcangulpak@gmail.com>
2024-03-31 13:37:19 +0200

    done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,25 @@
 // Your code here ...
+const { model, Schema } = require("mongoose");
+
+const recipeSchema = new Schema({
+  title: { type: String, required: true, unique: true },
+  instructions: { type: String, required: true },
+  level: {
+    type: String,
+    enum: ["Easy Peasy", "Amateur Chef", " UltraPro Chef"],
+  },
+  ingredients: { tpye: [String] }, // Ask this
+  image: {
+    type: String,
+    Default: " https://images.media-allrecipes.com/images/75131.jpg",
+  },
+  duration: { type: Number, min: 0 },
+
+  isArchived: { type: Boolean, default: false },
+  create: {
+    type: Date,
+    default: today,
+  },
+});
+
+module.exports = Recipe;
