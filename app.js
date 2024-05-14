3769eb8a36f97dac6338eafe359c8f2c2cfcd7eb
guzegui <guzmanegui@gmail.com>
2024-05-14 20:58:25 +0200

    Done Iteration 2

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 
-
 const app = express();
 
 // MIDDLEWARE
@@ -9,52 +8,77 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
 const mongoose = require("mongoose");
 
 const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
+const recipe = require("./models/Recipe.model");
+const Recipe = require("./models/Recipe.model");
+
 mongoose
   .connect(MONGODB_URI)
-  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
   .catch((err) => console.error("Error connecting to mongo", err));
-
-
 
 // ROUTES
 //  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
 
+app.post("/", (req, res) => {
+  const {
+    title,
+    instructions,
+    level,
+    ingredients,
+    image,
+    duration,
+    isArchived,
+    created,
+  } = req.body;
+
+  Recipe.create(
+    title,
+    instructions,
+    level,
+    ingredients,
+    image,
+    duration,
+    isArchived,
+    created
+  )
+    .then((response) => {
+      console.log("Entry created ->", response);
+      res.status(201).json(response);
+    })
+    .catch((error) => {
+      console.error("Error while creating entry ->", error);
+      res.status(500).json({ error: "Failed to create the entry" });
+    });
+});
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
 
-
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
 
-
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
-
-
 // Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
+app.listen(3000, () => console.log("My first app listening on port 3000!"));
 
 //❗️DO NOT REMOVE THE BELOW CODE
 module.exports = app;
