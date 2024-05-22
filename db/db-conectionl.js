8a758d6cc4fe34a46476e3b36e09e28f4a55f17c
CristinaColomoiets <colomoetscristina@gmail.com>
2024-05-22 22:59:57 +0200

    Lab completed

---
+++ db/db-conectionl.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+    .connect(MONGODB_URI)
+    .then((x)=> console.log(`Connected to Mongo! Database name: "${x.conections[0].name}"`))
+    .catch((err)=> console.error("Error connecting to mongo", err))
+    
