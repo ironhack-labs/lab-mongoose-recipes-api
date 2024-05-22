1e379ceb5c427a95e40746636198543e9f9fbda5
Ronnie Vélez <ronnievelz@gmail.com>
2024-05-22 15:38:59 +0200

    completed lab

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
-
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 const app = express();
 
 // MIDDLEWARE
@@ -14,6 +15,11 @@ app.use(express.json());
 
 
 
+
+  mongoose.connect("mongodb://127.0.0.1:27017/express-mongoose-recipes-dev")
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
+
 // ROUTES
 //  GET  / route - This is just an example route
 app.get('/', (req, res) => {
@@ -23,22 +29,77 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
-
+app.post("/recipes",(req, res)=>{
+Recipe.create({
+    name:req.body.name,
+    instructions:req.body.instructions,
+    level:req.body.level,
+    ingredients:req.body.ingredients,
+    image:req.body.image,
+    duration:req.body.duration,
+    isArchived:req.body.isArchived,
+    created:req.body.created
+})
+.then(()=>{
+    res.json({message:"todo esta bien"})
+}).catch((error)=>{
+    res.json(error)
+})
+})
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get("/recipes",async(req,res)=>{
+try {
+    const response =await Recipe.find()
+    console.log(response);
+    res.json(response)
+    
+} catch (error) {
+    res.json(error)
+}
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get("/recipes/:recipesId", async(req,res)=>{
+    console.log(req.params);
+    try {
+        const response= await Recipe.findById(req.params.recipesId)
+        res.json(response)
+    } catch (error) {
+        res.json(error)
+    }
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put("/recipes/:recipesId",async(req,res)=>{
+    try {
+        await Recipe.findByIdAndUpdate(req.params.recipesId,{
+        name:req.body.name,
+        instructions:req.body.instructions,
+        level:req.body.level,
+        ingredients:req.body.ingredients,
+        image:req.body.image,
+        duration:req.body.duration,
+        isArchived:req.body.isArchived,
+        created:req.body.created
+    })
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
+app.delete("/recipes/:recipesId", async(req,res)=>{
+    try {
+        await Recipe.findByIdAndDelete(req.params.recipesId)
+        res.json("receta borrada")
+    } catch (error) {
+        console.log(error);
+    }
+})
 
 
 
