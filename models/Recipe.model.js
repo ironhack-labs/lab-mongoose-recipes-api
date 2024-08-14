e50df2ef39b6389eb2284e368b494734a03fe00e
Aquiles <Aquileshr92@gmail.com>
2024-08-14 15:45:42 +0200

    lab done

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,45 @@
-// Your code here ...
+const mongoose = require('mongoose');
+
+
+// Define the Recipe schema
+const recipeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  instructions: {
+    type: String,
+    required: true
+  },
+  level: {
+    type: String,
+    enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+    required: true
+  },
+  ingredients: {
+    type: [String], // Array of strings
+    required: true
+  },
+  image: {
+    type: String,
+    default: 'https://images.media-allrecipes.com/images/75131.jpg'
+  },
+  duration: {
+    type: Number,
+    min: 0
+  },
+  isArchived: {
+    type: Boolean,
+    default: false
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Create the Recipe model
+const Recipe = mongoose.model('Recipe', recipeSchema);
+
+module.exports = Recipe;
