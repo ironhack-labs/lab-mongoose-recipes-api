0acd7c2fc7d2bd6988d6e90c3022bd0148ecb578
antonia trombetta <antonia.dorazio@gmail.com>
2024-08-28 18:22:01 +0200

    done

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 
 const app = express();
+
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+
 
 // MIDDLEWARE
 app.use(logger("dev"));
@@ -11,6 +17,11 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 
 
@@ -24,23 +35,82 @@ app.get('/', (req, res) => {
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
 
+app.post("/recipes", (req, res)=>{
+
+    Recipe.create({
+        titel: req.body.title,
+        instructions: req.body.instructions, 
+        level: req.body.level,
+        ingredients: req.body.ingredients,
+        image: req.body.image, 
+        duration: req.body.duration,
+        isArchived: req.body.isArchived,
+        created: req.body.created,
+    })
+    .then((createdRecipe)=>{
+     res.status(201).json(createdRecipe)
+    })
+    .catch(e => {
+     res.status(500).json({error:"failed to create a new recipe.."});   
+    });
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
 
+app.get("/recipes", (req, res)=>{
+
+    Recipe.find()
+    .then((allRecipes)=>{
+     res.status(200).json(allRecipes)
+    })
+    .catch(e => {
+        res.status(500).json({error:"failed to create a new recipe.."});   
+       });
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+
+app.get("recipes/:id", (req, res)=>{
+
+Recipe.findOne(req.params.id)
+.then((recipe)=>{
+  res.status(200).json(recipe);
+})
+.catch(e => {
+    res.status(500).json({error:"failed to create a new recipe.."});   
+   });
+
+})
 
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
 
+app.put("/recipes/:id", (req, res)=>{
+    Recipe.findOneAndUpdate(req.params.id, req.body, {new:true})
+    .then((updatedRecipe)=>{
+     res.status(200).json(updatedRecipe)
+    })
+    .catch(e => {
+        res.status(500).json({error:"failed to create a new recipe.."});   
+       });
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
+app.delete("/recipes/:id", (req, res)=>{
 
+    Recipe.findByIdAndDelete(req.params.id)
+    .then( ()=>{
+        res.status(204).send()
+    })
+    .catch(e => {
+        res.status(500).json({error:"failed to create a new recipe.."});   
+       });
+})
 
 // Start the server
 app.listen(3000, () => console.log('My first app listening on port 3000!'));
