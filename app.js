3b03077ee8ecaad37f0aebd95ced5cf833993541
as-Solo <asdelsolo@gmail.com>
2024-10-09 15:31:09 +0200

    Completed lab

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+
+const mongoose = require("mongoose")
+
 const express = require("express");
 const logger = require("morgan");
 
@@ -11,8 +14,11 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+mongoose.connect("mongodb://127.0.0.1:27017/express-mongoose-recipes-dev")
+.then((x)=>console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+.catch(error=>console.log(error))
 
-
+const Recipe = require("./models/Recipe.model")
 
 // ROUTES
 //  GET  / route - This is just an example route
@@ -23,23 +29,42 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post('/recipes', (req, res)=>{
+    Recipe.create({...req.body})
+    res.send(`The recipe: ${req.body.title} has been added.`)
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get('/recipes', (req, res)=>{
+    Recipe.find()
+    .then(response=>res.json(response))
+    .catch(error=>console.log(error))
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get('/recipes/:recipeId', (req, res)=>{
+    Recipe.findById(req.params.recipeId)
+    .then(response=>res.json(response))
+    .catch(error=>console.log(error))
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put('/recipes/:recipeId', (req, res)=>{
+    Recipe.findByIdAndUpdate(req.params.recipeId, {...req.body})
+    .then(response=>res.json(response))
+    .catch(error=>console.log(error))
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.delete('/recipes/:recipeId', (req, res)=>{
+    Recipe.findByIdAndDelete(req.params.recipeId)
+    .then(response=>console.log(`La receta ${response.title} ha sido eliminada.`))
+    .catch(error=>console.log(error))
+})
 
 
 // Start the server
