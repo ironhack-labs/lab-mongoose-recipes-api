9937cfed780ace1f5f0ef037b8f07eeba8d8c6ec
unknown <maludietrich.00@gmail.com>
2024-10-09 18:35:53 +0200

    done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 
 const app = express();
+const mongoose = require('mongoose')
 
 // MIDDLEWARE
 app.use(logger("dev"));
@@ -11,6 +12,13 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+    .connect(MONGODB_URI)
+    .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch((err) => console.error("Error connecting to mongo", err));
 
 
 
@@ -23,23 +31,80 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post('/recipes', async (req, res) => {
+    try {
+        const createdRecipe = await Recipe.create({
+            title: req.body.title,
+            instructions: req.body.instructions,
+            level: req.body.level,
+            ingredients: req.body.ingredients,
+            image: req.body.image,
+            duration: req.body.duration,
+            isArchived: req.body.isArquived,
+            created: req.body.created
+        })
+        res.status(201).json(createdRecipe)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
 
+app.get('/recipes', async (req, res) => {
+    try {
+        const allRecipes = await Recipe.find()
+        res.status(200).json(allRecipes)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get('/recipes/:id', async (req, res) => {
+    try {
+        const singleRecipe = await Recipe.findById(req.params.id)
+        res.status(200).json(singleRecipe)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put('/recipes/:id', async (req, res) => {
+    try {
+        const updateRecipe = await Recipe.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            instructions: req.body.instructions,
+            level: req.body.level,
+            ingredients: req.body.ingredients,
+            image: req.body.image,
+            duration: req.body.duration,
+            isArchived: req.body.isArquived,
+            created: req.body.created
+        }, { new: true })
+        res.status(200).json(updateRecipe)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
+app.delete('recipes/:id', async (req, res) => {
+    try {
 
+        await Recipe.findByIdAndDelete(req.params.id)
+        res.status(204).send()
+
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
 
 
 // Start the server
