c313830b037bcb5793740ff2b15dee1e0d153dc5
Ana Badolato <badolatoana@gmail.com>
2024-10-09 17:44:12 +0200

    lab done

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require('./models/Recipe.model');
 
 const app = express();
 
@@ -11,34 +13,111 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
+
+// ...
 
 
 
 // ROUTES
 //  GET  / route - This is just an example route
+app.post("/recipes", (req, res) => {
+
+    Recipe.create({
+        title: req.body.title,
+        instructions: req.body.instructions,
+        level: req.body.level,
+        ingredients: req.body.ingredients,
+        image: req.body.image,
+        duration: req.body.duration,
+        isArchived: req.body.isArchived,
+        created: req.body.created
+    })
+    .then((createdRecipe)=>{
+        res.status(201). json(createdRecipe);
+    })
+    .catch((err)=>{
+        res.status(500). json({message:"Error creating new recipe"});
+    })
+
+})
+
+
+app.get("/recipes", async (req, res) => {
+    Recipe.find()
+    try {
+      const recipes = await Recipe.find(); 
+      res.status(200).json(recipes); 
+    } catch (err) {
+      console.error("Error retrieving recipes:", err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+
+
+
+
+app.get("/recipes/:id", async (req, res) => {
+    try {
+      const recipe = await Recipe.findById(req.params.id); 
+      res.status(200).json(recipe); 
+    } catch (err) {
+      console.error("Error retrieving recipe:", err);
+      res.status(500).json({ message: "Internal server error" }); 
+    }
+  });
+
+
+
+// PUT /recipes/:id - Update a specific recipe by its _id
+app.put("/recipes/:id", async (req, res) => {
+    try {
+      const updatedRecipe = await Recipe.findByIdAndUpdate(
+        req.params.id,
+        {
+          title: req.body.title,
+          instructions: req.body.instructions,
+          level: req.body.level,
+          ingredients: req.body.ingredients,
+          image: req.body.image,
+          duration: req.body.duration,
+          isArchived: req.body.isArchived,
+          created: req.body.created,
+        },
+        { new: true } 
+      );
+  
+      res.status(200).json(updatedRecipe); 
+    } catch (err) {
+      console.error("Error updating recipe:", err);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  });
+  
+
+ 
+app.delete("/recipes/:id", async (req, res) => {
+    try {
+      await Recipe.findByIdAndDelete(req.params.id); 
+      res.status(204).send(); 
+    } catch (err) {
+      console.error("Error deleting recipe:", err);
+      res.status(500).json({ message: "Internal server error" }); 
+    }
+  });
+  
+
+
 app.get('/', (req, res) => {
     res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
 
 
-//  Iteration 3 - Create a Recipe route
-//  POST  /recipes route
-
-
-//  Iteration 4 - Get All Recipes
-//  GET  /recipes route
-
-
-//  Iteration 5 - Get a Single Recipe
-//  GET  /recipes/:id route
-
-
-//  Iteration 6 - Update a Single Recipe
-//  PUT  /recipes/:id route
-
-
-//  Iteration 7 - Delete a Single Recipe
-//  DELETE  /recipes/:id route
 
 
 
