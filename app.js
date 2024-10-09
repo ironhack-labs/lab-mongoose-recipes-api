582dff45cae5eb152512c895a73d744eaf1cb100
alberto marcos <amarcosarranz@gmail.com>
2024-10-09 21:14:39 +0200

    FT_RMT_2408_ES Alberto Marcos

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ app.put("/recipes/:id", (req, res) => {
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-app.put("/recipes/:id", (req, res) => {
+app.delete("/recipes/:id", (req, res) => {
   Recipe.findByIdAndDelete(req.params.id)
     .then(() => {
       res.status(204).send();
