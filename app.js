1ab560b58fde7779579defbb9e888cdcbca5292c
Diego Lázaro <dlazarodecastro@gmail.com>
2024-10-09 14:37:07 +0200

    lab done

--- app.js
+++ app.js
@@ -11,10 +11,19 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const mongoose = require("mongoose");
+
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
+
+mongoose.connect(MONGODB_URI)
+    .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+    .catch((err) => console.error("Error connecting to mongo", err));
 
 
 
 // ROUTES
+
+const Recipe = require("./models/Recipe.model.js")
 //  GET  / route - This is just an example route
 app.get('/', (req, res) => {
     res.send("<h1>LAB | Express Mongoose Recipes</h1>");
@@ -23,23 +32,78 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", async (req, res) => {
+    try {
+        const response = await Recipe.create({
+            title: req.body.title,
+            instructions: req.body.instructions,
+            level: req.body.level,
+            ingredients: req.body.ingredients,
+            image: req.body.image,
+            duration: req.body.duration,
+            isArchived: req.body.isArchived,
+            created: req.body.created
+        })
+        res.status(201).json(response)
+    } catch (error) {
+        res.status(500).json({message: "Error creando"})
+    }
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
+app.get("/recipes", async (req, res) => {
+    try {
+        const response = await Recipe.find()
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({message: "Error al recibir data"})
+    }
+})
 
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
+app.get("/recipes/:id", async (req, res) => {
+    try {
+        const response = await Recipe.findById(req.params.id)
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({message: "Error al recibir data"})
+    }
+})
 
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put("/recipes/:id", async (req, res) => {
+    try {
+        const response = await Recipe.findByIdAndUpdate(req.params.id, {
+            title: req.body.title,
+            instructions: req.body.instructions,
+            level: req.body.level,
+            ingredients: req.body.ingredients,
+            image: req.body.image,
+            duration: req.body.duration,
+            isArchived: req.body.isArchived,
+            created: req.body.created
+        }, {new: true})
+        res.status(200).json(response)
+    } catch (error) {
+        res.status(500).json({message: "Error al editar data"})
+    }
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.delete("/recipes/:id", async (req, res) => {
+    try {
+        await Recipe.findByIdAndDelete(req.params.id)
+        res.status(204).send()
+    } catch (error) {
+        res.status(500).json({message: "Error al borrar data"})
+    }
+})
 
 
 // Start the server
