c8fe884b63ce738cfec2a5d4d005ea9e0e2c7109
r.ruizfer <r.ruizfer97@gmail.com>
2024-10-09 15:52:32 +0200

    done

--- app.js
+++ app.js
@@ -8,44 +8,100 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const mongoose = require("mongoose");
 
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev";
 
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // ROUTES
 //  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
 
-
 //  Iteration 3 - Create a Recipe route
+const Recipe = require("./models/Recipe.model.js");
 //  POST  /recipes route
-
-
+app.post("/recipes", async (req, res) => {
+  try {
+    const response = await Recipe.create({
+      title: req.body.title,
+      instructions: req.body.instructions,
+      level: req.body.level,
+      ingredients: req.body.ingredients,
+      image: req.body.image,
+      duration: req.body.duration,
+      isArchived: req.body.isArchived,
+      created: req.body.created,
+    });
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ message: " Error creando receta" });
+  }
+});
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
-
+app.get("/recipes", async (req, res) => {
+  try {
+    const response = await Recipe.find();
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ message: " Error encontrando recetas" });
+  }
+});
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
-
+app.get("/recipes/:id", async (req, res) => {
+  try {
+    const response = await Recipe.findById(req.params.id);
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ message: " Error encontrando receta" });
+  }
+});
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
-
+app.put("/recipes/:id", async (req, res) => {
+  try {
+    const response = await Recipe.findByIdAndUpdate(
+      req.params.id,
+      {
+        title: req.body.title,
+        instructions: req.body.instructions,
+        level: req.body.level,
+        ingredients: req.body.ingredients,
+        image: req.body.image,
+        duration: req.body.duration,
+        isArchived: req.body.isArchived,
+        created: req.body.created,
+      },
+      { new: true }
+    );
+    res.status(201).json(response);
+  } catch (error) {
+    res.status(500).json({ message: " Error actualizando receta" });
+  }
+});
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
-
-
+app.delete("/recipes/:id", async (req, res) => {
+    try {
+      await Recipe.findByIdAndDelete(req.params.id);
+      res.status(204).send("Receta borrada");
+    } catch (error) {
+      console.log(error);
+    }
+  });
 // Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
+app.listen(3000, () => console.log("My first app listening on port 3000!"));
 
 //❗️DO NOT REMOVE THE BELOW CODE
 module.exports = app;
