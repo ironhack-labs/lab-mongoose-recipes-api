806cf2ebe3c784848e92e10e7c50e39b8eb1a4c7
Lena Cortes <lena.cortes@usal.es>
2025-01-14 14:44:38 +0100

    done

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-
+const Recipe = require("./models/Recipe.model")
 const app = express();
 
 // MIDDLEWARE
@@ -30,23 +30,79 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
-
+app.post("/recipes", async (request, response) => {
+    try {
+        const newRecipe = await Recipe.create(request.body); //for this we need const app = express()
+        response.status(201).json(newRecipe);
+    } catch (error) {
+        console.log(error);
+        response.status(500).json(error);
+    }
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
-
+app.get('/recipes', async (request, response) => {
+    try {
+        const recipes = await Recipe.find()
+        response.json(recipes)
+    } catch (error) {
+        console.log(error)
+        response.status(500).json(error)
+    }
+})
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get("/recipes/:id", async (request, response) => {
+    const { id } = request.params
+    if (mongoose.isValidObjectId(id)) {
+        try {
+            const oneRecipe = await Recipe.findById(id)
+            response.status(200).json(oneRecipe)
+        } catch (error) {
+            console.log(error);
+            response.status(500).json(error);
+        }
+    } else {
+        response.status(400).json({ message: 'Invalid Id' })
+    }
+})
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
+app.put("/recipes/:id", async (request, response) => {
 
+    const { id } = request.params
+
+    try {
+        const updatedRecipe = await Recipe.findByIdAndUpdate(id, request.body, {
+            new: true,
+            runValidators: true,
+        })
+        response.status(200).json(updatedRecipe)
+    } catch (error) {
+        console.log(error);
+        response.status(500).json(error)
+    }
+})
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.delete("/recipes/:id", async (request, response) => {
+    const { id } = request.params
+    if (mongoose.isValidObjectId(id)) {
+        try {
+            await Recipe.findByIdAndDelete(id)
+            response.status(204).json()
+        } catch (error) {
+            console.log(error);
+            response.status(500).json(error);
+        }
+    } else {
+        response.status(400).json({ message: 'Invalid Id' })
+    }
+})
 
 
 // Start the server
