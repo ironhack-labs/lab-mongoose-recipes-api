9e2ee53fc59cd4b7b7524e30eb2f4cd51bd7303e
martxgomez <martagomezmartinez95@gmail.com>
2025-01-25 12:25:51 +0100

    Iteration 1 done

--- app.js
+++ app.js
@@ -11,7 +11,18 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
+const mongoose = require ("mongoose")
+// app.js
+//...
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
+
+// ...
 
 
 // ROUTES
