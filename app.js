9b862aac7990010227ef78f77527729790620f25
martxgomez <martagomezmartinez95@gmail.com>
2025-01-25 13:01:12 +0100

    Iteration 3 done

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ app.use(logger("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
-const mongoose = require ("mongoose")
+const mongoose = require("mongoose");
 // app.js
 //...
 
@@ -19,44 +18,54 @@ const MONGODB_URI = process.env.MONGODB_URI;
 
 mongoose
   .connect(MONGODB_URI)
-  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
   .catch((err) => console.error("Error connecting to mongo", err));
 
 // ...
 
-
 // ROUTES
 //  GET  / route - This is just an example route
-app.get('/', (req, res) => {
-    res.send("<h1>LAB | Express Mongoose Recipes</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1>LAB | Express Mongoose Recipes</h1>");
 });
 
-
 //  Iteration 3 - Create a Recipe route
-//  POST  /recipes route
+app.post("/recipes", (req, res) => {
+  Recipe.create({
+    title: req.body.title,
+    instructions: req.body.instructions,
+    level: req.body.level,
+    ingredients: req.body.ingredientes,
+    image: req.body.image,
+    duration: req.body.duration,
+    isArchived: req.body.isArchived,
+    created: req.body.created,
+  })
 
+    .then((createdRecipe) => {
+      res.status(201).json(createdRecipe);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: "Error while creating a new recipe" });
+    });
+});
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
 
-
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
 
-
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
 
-
-
 // Start the server
-app.listen(3000, () => console.log('My first app listening on port 3000!'));
-
-
+app.listen(3000, () => console.log("My first app listening on port 3000!"));
 
 //❗️DO NOT REMOVE THE BELOW CODE
 module.exports = app;
