3714a5f319a9fab24aa74001a377e0c34d45fd37
Eleni Kriezia <173841123+nthTimeIsTheCharm@users.noreply.github.com>
2025-01-25 12:48:21 +0100

    iteration 2

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,63 @@
-// Your code here ...
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const recipeSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: [true, "Title is required"],
+            trim: true,
+            unique: true
+        }
+    },
+    {
+        instructions: {
+            type: String,
+            required: [true, "Instructions are required"],
+            trim: true,
+        }
+    },
+    {
+        level: {
+            type: [String],
+            required: [true, "Level is required"],
+            enum: ["Easy Peasy" - "Amateur Chef" - "UltraPro Chef"] 
+        }
+    },
+    {
+        ingredients: {
+            type: String,
+            required: [true, "Ingredients are required"],
+            trim: true,
+        }
+    },
+    {
+        image: {
+            type: String,
+            default: "https://images.media-allrecipes.com/images/75131.jpg",
+            trim: true,
+        }
+    },
+    {
+        duration: {
+            type: Number,
+            required: [true, "Duration is required"],
+            min: 0,
+        }
+    },
+    {
+        isArchived: {
+            type: Boolean,
+            default: false,
+        }
+    },
+    {
+        created: {
+            type: Date,
+            default: Date.now,
+        }
+    },
+);
+
+module.exports = Recipe;
