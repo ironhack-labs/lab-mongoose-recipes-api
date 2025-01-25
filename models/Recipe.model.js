05b55b948c5851efdfd8f01544a82c1f6a619475
hurtaditoo <al415507@uji.es>
2025-01-25 14:25:05 +0100

    Iteration 2 - Recipe Model

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,1 +1,59 @@
 // Your code here ...
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({ // Schema es un objeto que define la estructura de los eventos
+    title: {
+        type: String,
+        require: [true, 'Event title is required'],  // Si no se pone un título, salta un error 
+        unique: true,    
+    },
+
+    instructions: {
+        type: String,
+        require: [true, 'Recipe instruction are required'],
+    },
+
+    level: {
+        type: String,
+        enum: ['Easy Peasy', 'Amateur Chef', 'UltraPro Chef'],
+    },
+
+    ingredients: {
+        type: [ String ],
+    },
+
+    image: {
+        type: String,
+        default: "https://images.media-allrecipes.com/images/75131.jpg",
+    },
+
+    duration: {
+        type: Number,
+        min: 0,
+    },
+
+    isArchived: {
+        type: Boolean,
+        default: false,
+    },
+
+    created: {
+        type: Boolean,
+        default: Date.now(),
+    }
+
+}, {
+    timestamps: true,
+    toJSON: {
+        transform: function (doc, ret) {
+        delete ret.__v;
+        delete ret._id;
+        ret.id = doc.id;
+        return ret;
+        }
+    }
+});
+
+
+const Recipe = mongoose.model('Recipe', eventSchema); // Model crea un objeto que se corresponde con modelo definido anteriormente
+module.exports = Recipe; 
