60bb0094e168e70a68550fc650b2375fa95610f3
Julia Solias <juliasohu@gmail.com>
2025-02-26 15:25:19 +0100

    Create Recipe functionality - it 3

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 
 const app = express();
 
@@ -35,6 +36,25 @@ app.get('/', (req, res) => {
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
 
+app.post('/recipes', (req, res) => {
+    Recipe.create(
+        {
+            title: req.body.title,
+            instructions: req.body.instructions,
+            level: req.body.level,
+            ingredients: req.body.ingredients,
+            image: req.body.image,
+            duration: req.body.duration,
+            isArchived: req.body.isArchived,
+            created: req.body.created
+        }
+    )
+    .then((createdRecipe) => {
+        res.status(201).json(createdRecipe)
+    }
+    )
+    .catch(e => res.status(500).json({message: "Error when creating new recipe"}))
+})
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
