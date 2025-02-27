d2c20cc4249084c79e80fe08244bfc7304538150
Patricia Gracia Gutierrez <pgg.uxui@gmail.com>
2025-02-27 17:14:24 +0000

    Completed lab

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -5,14 +5,15 @@ const Schema = mongoose.Schema;
 // CREATE SCHEMA
 const recipeSchema = new Schema({
     title: {type: String, required:true , unique: true},
-    instructions: {type: String, required:true},
     level: { type: String, enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"] },
     ingredients: { type: [String] },
+    cuisine: {type: String, required: true}, // Añadido
+    dishType: { type: String, enum: ["breakfast", "main_course", "soup", "snack", "drink", "dessert", "other"] }, // Añadido
     image: { type: String, default: "https://images.media-allrecipes.com/images/75131.jpg" },
     duration: { type: Number, min: 0 },
-    isArchived: { type: Boolean, default: false },
+    creator: { type: mongoose.ObjectId, ref: "User" }, // Añadido
     created: { type: Date, default: Date.now }
-});
+  });
 
 // CREATE MODEL
 const Recipe = mongoose.model("Recipe", recipeSchema);
